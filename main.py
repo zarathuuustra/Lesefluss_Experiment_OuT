38d62cb407a9fc3c519bfb07dcf49de0984e4568
Timur B. <T.Berenstein@gmx.de>
2025-06-26 14:47:24 +0200

    Added buttons and a possibility to enter data.

--- main.py
+++ main.py
@@ -15,35 +15,41 @@ def button_action():
 
 fenster = Tk()
 
-# Den Fenstertitle erstellen
-
-fenster.title("Experiment TEXT UND KOHAERENZ")
-
+fenster.title("Experiment TEXT UND KOHAERENZ") # Titel des Fensters
+fenster.geometry('1000x750') # Wie groß das Fenster ist
 
 # Label und Buttons erstellen.
 
 change_button = Button(fenster, text="Ändern", command=button_action)
-
 exit_button = Button(fenster, text="Beenden", command=fenster.quit)
+# button widget with red color text inside
 
 
-anweisungs_label = Label(fenster, text="Ich bin eine Anweisung:\n\ klicke auf 'Ändern'.")
+anweisungs_label = Label(fenster, text="Ich bin eine Anweisung:\nklicke auf 'Ändern'.")
+info_label = Label(fenster, text="Ich bin eine Info:\n Der Beenden Button schliesst das Programm.")
+ergebnis_label = Label(fenster, text="Du hast noch nichts geschrieben! Schreibe doch etwas")
+
+txt = Entry(fenster, width=10)
+# function to display user text when
+# button is clicked
+def clicked():
+    res = "You wrote " + txt.get()
+    ergebnis_label.configure(text = res)
+
+btn = Button(fenster, text = "RESULT" ,fg = "red", command=clicked)
+# Nun fügen wir die Komponenten unserem Fenster in der gewünschten Reihenfolge hinzu.
+# Note: there are different geometry management tools: pack(), grid(), place(); grid could help keep them in plae
+
+anweisungs_label.pack() # 1. Label anzeigen mit Text
+change_button.pack() # 2. Button darunter hinzufügen. Er benutzt die Funktion: "Button Action"
+info_label.pack() # 3. Label (wie in 1)
+ergebnis_label.pack()
+txt.pack()
+btn.pack()
+exit_button.pack() # 4. Button wieder hinzufügen. Er benutz die Funktion "Quit" --> Schließt das Fenster
 
 
-info_label = Label(fenster, text="Ich bin eine Info:\n\ Der Beenden Button schliesst das Programm.")
 
-
-# Nun fügen wir die Komponenten unserem Fenster
-
-# in der gwünschten Reihenfolge hinzu.
-
-anweisungs_label.pack()
-
-change_button.pack()
-
-info_label.pack()
-
-exit_button.pack()
 
 
 # In der Ereignisschleife auf Eingabe des Benutzers warten.
